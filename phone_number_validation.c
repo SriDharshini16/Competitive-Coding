6ae19f20f3b6b02d5792332db8f7708be44d6725
SriDharshini16 <104094322+SriDharshini16@users.noreply.github.com>
2023-01-31 00:10:12 +0530

    Create phone_number_validation.c

---
+++ phone_number_validation.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+int main() {
+    long int a;
+    scanf("%ld",&a);
+    if(a>6000000000 && a<10000000000){
+      printf("valid phone number");
+    }
+    else {
+        printf("invalid phone number");
+    }
+
+    return 0;
+}
+
+(or)
+
+
+#include <stdio.h>
+
+int main() {
+    long int a;
+    int arr[10];
+    scanf("%ld",&a);
+    if(a>=1000000000 && a<=9999999999){
+        for(int i = 0;i<10;i++){
+            int c = a%10;
+            arr[i]=c;
+            a=a/10;
+        }
+        if(arr[9]>=6 && arr[9]<=9){
+        printf("valid phone number");
+        }
+    }
+    else {
+        printf("invalid phone number");
+    }
+
+    return 0;
+}
+
