160e0331c59f80de6f9fdf27d11721e04195096f
SriDharshini16 <104094322+SriDharshini16@users.noreply.github.com>
2023-01-31 23:01:30 +0530

    Update reversing_number.c

--- reversing_number.c
+++ reversing_number.c
@@ -1,4 +1,4 @@
-//using modulo and divide operator
+//using modulo and divide operator for printing in reverse order
 #include <stdio.h>
 
 int main() {
@@ -9,5 +9,22 @@ int main() {
     for(a;a>=1;a=a/10){
         printf("%d",a%10);
     }
+    return 0;
+}
+
+//reversing a number and checking for palindrome
+#include <stdio.h>
+
+int main() {
+    int num,remain,reverse=0;
+    printf("enter any number");
+    scanf("%d",&num);
+    int original =num;
+    for(num;num>=1;num=num/10){
+        remain=num%10;
+        reverse= reverse*10+remain;
+    }
+    printf("%d \n",reverse);
+    original==reverse ? printf("palindrome"):printf("not a palindrome");
     return 0;
 }
